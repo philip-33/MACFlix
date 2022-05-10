bacf5168ab092d24d038667b476aeaed6a732935
philip-33 <22357184+philip-33@users.noreply.github.com>
2022-05-09 21:08:10 -0700

    Installed Axios, confirmed .env functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
 import './App.css';
 
-/*
-Firebase slug
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: "macflix-fb832.firebaseapp.com",
-  projectId: "macflix-fb832",
-  storageBucket: "macflix-fb832.appspot.com",
-  messagingSenderId: "199641251105",
-  appId: "1:199641251105:web:ecd121d1d6b064101cd260"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-*/
-
 function App() {
   return (
     <div className="App">
