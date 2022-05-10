5c17eebbadbcc94a10fe99145103c8f5236c8a9a
philip-33 <22357184+philip-33@users.noreply.github.com>
2022-05-09 20:23:17 -0700

    Firebase setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import './App.css';
 
+/*
+Firebase slug
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6xiex8KLElA9sVM1aBE2UH7EYuAfgVDI",
+  authDomain: "macflix-fb832.firebaseapp.com",
+  projectId: "macflix-fb832",
+  storageBucket: "macflix-fb832.appspot.com",
+  messagingSenderId: "199641251105",
+  appId: "1:199641251105:web:ecd121d1d6b064101cd260"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+*/
+
 function App() {
   return (
     <div className="App">
